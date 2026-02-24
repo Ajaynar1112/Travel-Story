ab11e0ae89e017537a7b3d6d748e80e09d56b9c8
Ajaynar1112 <ajaynarra286@gmail.com>
2026-02-24 10:13:35 +0530

    Initial commit: project structure with frontend and backend

---
+++ Frontend/travel-story-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Home/Home";
+
+const App = () => {
+  return (
+    <div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+     </Router>
+      
+    </div>
+  );
+};
+
+export default App
